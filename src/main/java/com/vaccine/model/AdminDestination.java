cc39eaa0bdb4d5351f8b04f988805259450af25c
Binh <nguyenvanthaibinh2210@gmail.com>
2021-07-29 10:53:23 +0700

    update first time

---
+++ src/main/java/com/vaccine/model/AdminDestination.java
@@ -0,0 +1,33 @@
+package com.vaccine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@Table(name="vaccine_destinations")
+public class AdminDestination {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String nameDestination;
+
+
+    public AdminDestination(String nameDestination, WarehouseVaccine warehouseVaccine) {
+        this.nameDestination = nameDestination;
+        this.warehouseVaccine = warehouseVaccine;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "id_warehouse_vaccine")
+    private WarehouseVaccine warehouseVaccine;
+}
